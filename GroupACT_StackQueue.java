7539af50d8a116a92b5332fcb8295f6741fb6dc7
Azores-Mea <151590493+Azores-Mea@users.noreply.github.com>
2024-11-15 07:12:58 +0800

    Update GroupACT_StackQueue.java

--- GroupACT_StackQueue.java
+++ GroupACT_StackQueue.java
@@ -4,14 +4,14 @@ public class GroupACT_StackQueue {
     public static void main(String[] args) {
         Scanner user_input = new Scanner(System.in);
 
-        System.out.print("Enter array size: ");
+        System.out.print("ENTER ARRAY SIZE: ");
         int size = user_input.nextInt();
 
         int[] array = new int[size];
-        int front = -1, rear = -1;
+        int top = -1, front = -1, rear = -1;
 
         while (true) {
-            System.out.println("\nChoose an option:");
+            System.out.println("\nCHOOSE AN OPTION:");
             System.out.println("[1] STACK | [2] QUEUE | [3] EXIT\n");
             System.out.print("Enter your choice: ");
             int choice = user_input.nextInt();
@@ -20,62 +20,59 @@ public class GroupACT_StackQueue {
                 case 1:
                     while (true) {
                         System.out.println("\nStack Options:");
-                        System.out.println("[1] PUSH | [2] POP | [3] PEEK | [4] Check if FULL | [5] Check if EMPTY | [6] EXIT\n");
+                        System.out.println("[1] PUSH | [2] POP | [3] PEEK | [4] Check if FULL | [5] Check if EMPTY | [6] BACK TO MAIN OPTIONS\n");
                         System.out.print("Enter your choice: ");
                         int stackChoice = user_input.nextInt();
 
                         switch (stackChoice) {
-                            case 1: //PUSH
-                                if ((rear + 1) % size == front) {
-                                    System.out.println("Stack overflow. Cannot push.");
+                            case 1: // PUSH
+                                if (top == size - 1) {
+                                    System.out.println("STACK OVERFLOW. Unable to PUSH.");
                                 } else {
                                     System.out.print("Enter element to push: ");
                                     int element = user_input.nextInt();
-                                    if (front == -1) front = 0;
-                                    rear = (rear + 1) % size;
-                                    array[rear] = element;
+                                    array[++top] = element;
                                     System.out.println("Pushed: " + element);
-                                    display(array, front, rear, size);
+                                    display_stack(array, top);
                                 }
                                 break;
-                            case 2: //POP
-                               if (front == -1) {
-                                    System.out.println("Stack underflow. Cannot pop.");
+                            case 2: // POP
+                                if (top == -1) {
+                                    System.out.println("STACK UNDERFLOW. Unable to POP.");
                                 } else {
-                                    System.out.println("Popped: " + array[rear]);
-                                    if (front == rear) {
-                                        front = rear = -1;
-                                    } else {
-                                        rear = (rear - 1 + size) % size;
-                                    }
-                                    display(array, front, rear, size);
+                                    System.out.println("Popped: " + array[top--]);
+                                    display_stack(array, top);
                                 }
                                 break;
-                                
-                            case 3: //PEEK
-                                    if (front == -1) {
-                                    System.out.println("Stack is empty. Nothing to peek.");
+                            case 3: // PEEK
+                                if (top == -1) {
+                                    System.out.println("Stack is empty. Nothing to PEEK.");
                                 } else {
-                                    System.out.println("Top element: " + array[rear]);
-                                    display(array, front, rear, size);
+                                    System.out.println("Top element: " + array[top]);
+                                    display_stack(array, top);
                                 }
                                 break;
-                                
-                            case 4: //IsFull
-                                if ((rear + 1) % size == front) {
+                            case 4: // IsFull
+                                if (top == size - 1) {
                                     System.out.println("Stack is full.");
+                                    display_stack(array, top);
                                 } else {
                                     System.out.println("Stack is not full.");
+                                    display_stack(array, top);
                                 }
                                 break;
-                            case 5: //IsEmpty
-                                if (front == -1) {
+
+                            case 5: // IsEmpty
+                                if (top == -1) {
                                     System.out.println("Stack is empty.");
+                                    display_stack(array, top);
                                 } else {
                                     System.out.println("Stack is not empty.");
+                                    display_stack(array, top);
                                 }
                                 break;
-                            case 6: //EXIT | RETURN TO MAIN
+
+                            case 6: // EXIT | RETURN TO MAIN
                                 break;
                             default:
                                 System.out.println("Invalid Input.");
@@ -92,58 +89,61 @@ public class GroupACT_StackQueue {
                         int queueChoice = user_input.nextInt();
 
                         switch (queueChoice) {
-                            case 1: //ENQUEUE
+                            case 1: // ENQUEUE
                                 if ((rear + 1) % size == front) {
-                                    System.out.print("Queue is full, cannot add more elements.");
-                                }
-
-                                else {
+                                    System.out.println("QUEUE OVERFLOW, unable to add more elements.");
+                                } else {
                                     System.out.print("Enter element to queue: ");
                                     int value = user_input.nextInt();
-                                        if (front == -1) front = 0;
-                                        rear = (rear + 1) % size;
-                                        array[rear] = value;
-                                            System.out.println("Enqueued: " + value);
-                                            display (array, front, rear, size);
+                                    if (front == -1) front = 0;
+                                    rear = (rear + 1) % size;
+                                    array[rear] = value;
+                                    System.out.println("Enqueued: " + value);
+                                    display_queue(array, front, rear, size);
                                 }
                                 break;
-                           case 2: // DEQUEUE
-                            if (front == -1) {
-                                System.out.println("Queue is empty.");
-                            } else {
-                                System.out.println("Dequeueing the first element: " + array[front]);
-                                if (front == rear) { 
-                                    front = -1;
-                                    rear = -1;
+                            case 2: // DEQUEUE
+                                if (front == -1) {
+                                    System.out.println("QUEUE UNDERFLOW.");
                                 } else {
-                                    front = (front + 1) % size; 
+                                    System.out.println("Dequeueing the first element: " + array[front]);
+                                    if (front == rear) {
+                                        front = -1;
+                                        rear = -1;
+                                    } else {
+                                        front = (front + 1) % size;
+                                    }
+                                    display_queue(array, front, rear, size);
                                 }
-                                display(array, front, rear, size);
-                            }
-                            break;
-
-                            case 3: //PEEK
+                                break;
+                            case 3: // PEEK
                                 if (front == -1) {
                                     System.out.println("Queue is empty.");
+                                    display_queue(array, front, rear, size);
                                 } else {
                                     System.out.println("Front element: " + array[front]);
+                                    display_queue(array, front, rear, size);
                                 }
                                 break;
                             case 4: //IsFull
                             	if ((rear + 1) % size == front) {
                                     System.out.println("Queue is full.");
+                                    display_queue(array, front, rear, size);
                                 } else {
                                     System.out.println("Queue is not full.");
+                                    display_queue(array, front, rear, size);
                                 }
                                 break;
                             case 5: //IsEmpty
                             	if (front == -1) {
                                     System.out.println("Queue is empty.");
+                                    display_queue(array, front, rear, size);
                                 } else {
                                     System.out.println("Queue is not empty.");
+                                    display_queue(array, front, rear, size);
                                 }
                                 break;
-                            case 6: //EXIT | RETURN TO MAIN
+                            case 6: // EXIT | RETURN TO MAIN
                                 break;
                             default:
                                 System.out.println("Invalid Input.");
@@ -153,8 +153,8 @@ public class GroupACT_StackQueue {
                     break;
 
                 case 3:
-                    System.out.println("Exiting program.");
-                    System.exit(choice);
+                    System.out.println("Exiting program...");
+                    System.exit(0);
                     break;
 
                 default:
@@ -164,11 +164,23 @@ public class GroupACT_StackQueue {
         }
     }
 
-    public static void display(int[] array, int front, int rear, int size) {
-        if (front == -1) {
-            System.out.println("No elements to display.");
+    public static void display_stack(int[] array, int top) {
+        if (top == -1) {
+            System.out.println("\nNo elements in stack.");
         } else {
-            System.out.print("Elements: ");
+            System.out.println("\nStack elements: ");
+            for (int i = top; i >= 0; i--) {
+                System.out.println(array[i]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void display_queue(int[] array, int front, int rear, int size) {
+        if (front == -1) {
+            System.out.println("No elements to display in queue.");
+        } else {
+            System.out.print("Queue elements: ");
             int i = front;
             while (true) {
                 System.out.print(array[i] + " ");
